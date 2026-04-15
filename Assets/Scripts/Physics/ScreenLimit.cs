3e728311e01394efc055168234c7d6f5e4cffe61
JFLagar <juan_x_96@hotmail.com>
2026-04-15 10:47:15 +0200

    implemented Cinemachine and horizontal cameramovement

--- Assets/Scripts/Physics/ScreenLimit.cs
+++ Assets/Scripts/Physics/ScreenLimit.cs
@@ -5,20 +5,6 @@ public class ScreenLimit : MonoBehaviour
 {
     [SerializeField]
     private int screenEdgeFaceDir;
-    [SerializeField] 
-    private bool useRenderer;
-    [SerializeField]
-    private SpriteRenderer screenEdgeRenderer;
-
-    private void FixedUpdate()
-    {
-        if (!useRenderer)
-            return;
-        if (screenEdgeRenderer.isVisible)
-        {
-            Managers.Instance.CameraManager.SetWallDirection(screenEdgeFaceDir);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {

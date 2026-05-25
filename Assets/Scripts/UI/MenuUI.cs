71f639a7dbaec879d346ec7534bf7c88aa449981
JFLagar <juan_x_96@hotmail.com>
2026-05-25 12:13:09 +0200

    fixed ui bugs

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -10,24 +10,28 @@ public class MenuUI : MonoBehaviour , IMenuUI
     [SerializeField]
     private Selectable[] selectables;
 
+    private Selectable previousSelected;
+
     public virtual void OpenUIElement(int id)
     {
         foreach (RectTransform transform in uiElements)
         {
             transform.gameObject.SetActive(false);
         }
+        previousSelected = null;
         AudioManager.instance.PlaySoundEffect(1);
         uiElements[id].gameObject.SetActive(true);
-        if (selectables[id] != null)
+        if (id <= selectables.Length - 1 && selectables[id] != null)
         InputManager.Instance.GetMainPlayerController().SelectUIElement(selectables[id]);
     }
 
-    public virtual void CloseUIElements()
+    public virtual void CloseUIElements(Selectable selectable)
     {
         foreach (RectTransform transform in uiElements)
         {
             transform.gameObject.SetActive(false);
         }
+        selectable.Select();
     }
 }
 
